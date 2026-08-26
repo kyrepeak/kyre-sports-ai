dc38c876edc204bc2343c8b4a298eab26ded6c06
kyrepeak <kyrewhite15@gmail.com>
2026-08-26 13:17:36 -0400

    WNBA Step 5D: wire empirical outcome distribution router

--- sports_api/main.py
+++ sports_api/main.py
@@ -33,6 +33,7 @@ from sports_api.api.wnba_availability import router as wnba_availability_router
 from sports_api.api.wnba_baseline_projection import router as wnba_baseline_projection_router
 from sports_api.api.wnba_clutch_context import router as wnba_clutch_context_router
 from sports_api.api.wnba_defensive_activity import router as wnba_defensive_activity_router
+from sports_api.api.wnba_empirical_outcome_distribution import router as wnba_empirical_outcome_distribution_router
 from sports_api.api.wnba_event_lineup_context import router as wnba_event_lineup_context_router
 from sports_api.api.wnba_lineup_context import router as wnba_lineup_context_router
 from sports_api.api.wnba_live_game import router as wnba_live_game_router
@@ -90,6 +91,7 @@ app.include_router(wnba_availability_router)
 app.include_router(wnba_baseline_projection_router)
 app.include_router(wnba_clutch_context_router)
 app.include_router(wnba_defensive_activity_router)
+app.include_router(wnba_empirical_outcome_distribution_router)
 app.include_router(wnba_event_lineup_context_router)
 app.include_router(wnba_lineup_context_router)
 app.include_router(wnba_live_game_router)
