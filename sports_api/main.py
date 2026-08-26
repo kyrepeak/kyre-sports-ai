377f259efd231b8f2189ade6920ad8192f94939e
kyrepeak <kyrewhite15@gmail.com>
2026-08-26 11:16:51 -0400

    Wire WNBA matchup context router

--- sports_api/main.py
+++ sports_api/main.py
@@ -34,6 +34,7 @@ from sports_api.api.wnba_clutch_context import router as wnba_clutch_context_rou
 from sports_api.api.wnba_defensive_activity import router as wnba_defensive_activity_router
 from sports_api.api.wnba_lineup_context import router as wnba_lineup_context_router
 from sports_api.api.wnba_live_game import router as wnba_live_game_router
+from sports_api.api.wnba_matchup_context import router as wnba_matchup_context_router
 from sports_api.api.wnba_officiating_context import router as wnba_officiating_context_router
 from sports_api.api.wnba_rotation_context import router as wnba_rotation_context_router
 from sports_api.api.wnba_schedule_context import router as wnba_schedule_context_router
@@ -82,6 +83,7 @@ app.include_router(wnba_clutch_context_router)
 app.include_router(wnba_defensive_activity_router)
 app.include_router(wnba_lineup_context_router)
 app.include_router(wnba_live_game_router)
+app.include_router(wnba_matchup_context_router)
 app.include_router(wnba_officiating_context_router)
 app.include_router(wnba_rotation_context_router)
 app.include_router(wnba_schedule_context_router)
