f71e2b2527db521fbb85da2d8ef46e85988343d7
kyrepeak <kyrewhite15@gmail.com>
2026-08-26 16:13:11 -0400

    WNBA Step 5O: wire prop feed failover router

--- sports_api/main.py
+++ sports_api/main.py
@@ -52,6 +52,7 @@ from sports_api.api.wnba_pregame_prediction_store import router as wnba_pregame_
 from sports_api.api.wnba_projection_input_snapshot import router as wnba_projection_input_snapshot_router
 from sports_api.api.wnba_projection_scenarios import router as wnba_projection_scenarios_router
 from sports_api.api.wnba_prop_feed_collector import router as wnba_prop_feed_collector_router
+from sports_api.api.wnba_prop_feed_failover import router as wnba_prop_feed_failover_router
 from sports_api.api.wnba_prop_line_feed_adapter import router as wnba_prop_line_feed_adapter_router
 from sports_api.api.wnba_prop_threshold_probability import router as wnba_prop_threshold_probability_router
 from sports_api.api.wnba_rotation_context import router as wnba_rotation_context_router
@@ -120,6 +121,7 @@ app.include_router(wnba_pregame_prediction_store_router)
 app.include_router(wnba_projection_input_snapshot_router)
 app.include_router(wnba_projection_scenarios_router)
 app.include_router(wnba_prop_feed_collector_router)
+app.include_router(wnba_prop_feed_failover_router)
 app.include_router(wnba_prop_line_feed_adapter_router)
 app.include_router(wnba_prop_threshold_probability_router)
 app.include_router(wnba_rotation_context_router)
