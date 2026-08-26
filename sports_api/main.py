00b19705ccaf5f0dd97e2b6f4e6707d30fdaa89f
kyrepeak <kyrewhite15@gmail.com>
2026-08-26 14:12:35 -0400

    WNBA Step 5H: wire multi-sportsbook market consensus router

--- sports_api/main.py
+++ sports_api/main.py
@@ -41,6 +41,7 @@ from sports_api.api.wnba_live_game import router as wnba_live_game_router
 from sports_api.api.wnba_matchup_adjusted_projection import router as wnba_matchup_adjusted_projection_router
 from sports_api.api.wnba_matchup_context import router as wnba_matchup_context_router
 from sports_api.api.wnba_model_input_readiness import router as wnba_model_input_readiness_router
+from sports_api.api.wnba_multi_sportsbook_market_consensus import router as wnba_multi_sportsbook_market_consensus_router
 from sports_api.api.wnba_officiating_context import router as wnba_officiating_context_router
 from sports_api.api.wnba_player_event_features import router as wnba_player_event_features_router
 from sports_api.api.wnba_player_opportunity_context import router as wnba_player_opportunity_context_router
@@ -102,6 +103,7 @@ app.include_router(wnba_live_game_router)
 app.include_router(wnba_matchup_adjusted_projection_router)
 app.include_router(wnba_matchup_context_router)
 app.include_router(wnba_model_input_readiness_router)
+app.include_router(wnba_multi_sportsbook_market_consensus_router)
 app.include_router(wnba_officiating_context_router)
 app.include_router(wnba_player_event_features_router)
 app.include_router(wnba_player_opportunity_context_router)
