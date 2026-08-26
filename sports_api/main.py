b4075fe5fd3ec127729ce3a18816067ea3c0cdb5
kyrepeak <kyrewhite15@gmail.com>
2026-08-25 22:16:47 -0400

    Wire MLB environment router

--- sports_api/main.py
+++ sports_api/main.py
@@ -4,6 +4,7 @@ from sports_api.api.health import router as health_router
 from sports_api.api.mlb import router as mlb_router
 from sports_api.api.mlb_boxscore import router as mlb_boxscore_router
 from sports_api.api.mlb_bullpen import router as mlb_bullpen_router
+from sports_api.api.mlb_environment import router as mlb_environment_router
 from sports_api.api.mlb_game_logs import router as mlb_game_logs_router
 from sports_api.api.mlb_head_to_head import router as mlb_head_to_head_router
 from sports_api.api.mlb_recent_form import router as mlb_recent_form_router
@@ -29,6 +30,7 @@ app.include_router(mlb_head_to_head_router)
 app.include_router(mlb_starting_pitchers_router)
 app.include_router(mlb_slate_verification_router)
 app.include_router(mlb_bullpen_router)
+app.include_router(mlb_environment_router)
 
 
 @app.get("/", tags=["system"])
