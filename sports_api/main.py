0882d26927f251b680b518538bf9952e9682ff07
kyrepeak <kyrewhite15@gmail.com>
2026-08-26 02:27:51 -0400

    Wire WNBA rest and travel router

--- sports_api/main.py
+++ sports_api/main.py
@@ -32,6 +32,7 @@ from sports_api.api.wnba_advanced import router as wnba_advanced_router
 from sports_api.api.wnba_availability import router as wnba_availability_router
 from sports_api.api.wnba_lineup_context import router as wnba_lineup_context_router
 from sports_api.api.wnba_live_game import router as wnba_live_game_router
+from sports_api.api.wnba_schedule_context import router as wnba_schedule_context_router
 from sports_api.api.wnba_shot_context import router as wnba_shot_context_router
 from sports_api.api.wnba_standings import router as wnba_standings_router
 from sports_api.api.wnba_team_history import router as wnba_team_history_router
@@ -75,6 +76,7 @@ app.include_router(wnba_advanced_router)
 app.include_router(wnba_availability_router)
 app.include_router(wnba_lineup_context_router)
 app.include_router(wnba_live_game_router)
+app.include_router(wnba_schedule_context_router)
 app.include_router(wnba_shot_context_router)
 app.include_router(wnba_standings_router)
 app.include_router(wnba_team_history_router)
