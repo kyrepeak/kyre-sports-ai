4f637166ab61a7522ec5894049116269a09529fb
kyrepeak <kyrewhite15@gmail.com>
2026-09-02 16:56:51 -0700

    Route Matchup Explorer to cleanup Step 9 spotlight

--- mlb_matchup_hub_v27.py
+++ mlb_matchup_hub_v27.py
@@ -1,6 +1,6 @@
 """Compatibility entrypoint for the current MLB Matchup Explorer presentation."""
 from __future__ import annotations
 
-from mlb_matchup_hub_v48 import FROZEN_MATCHUP_CHAIN, VERSION, render_matchup_hub
+from mlb_matchup_hub_v49 import FROZEN_MATCHUP_CHAIN, VERSION, render_matchup_hub
 
 __all__ = ["FROZEN_MATCHUP_CHAIN", "VERSION", "render_matchup_hub"]
