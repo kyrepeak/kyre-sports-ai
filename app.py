7b0c448644ba7944649f6ea442d2f574375cf913
kyrepeak <kyrewhite15@gmail.com>
2026-09-20 18:57:40 -0700

    Activate universal shell at production entrypoint

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ V186_PRODUCTION_RUNTIME_REFRESH = "STREAMLIT_V186_NEXT_GAME_DATE_RUNTIME_REFRESH
 NFL_STEP7_APP_IDENTITY_RUNTIME = "STREAMLIT_V187_NFL_STEP7_APP_IDENTITY_FAIL_CLOSED_2026_09_20"
 PASSING_YARDS_LAYERED_PRODUCTION_RUNTIME = "STREAMLIT_V188_PASSING_YARDS_LAYERED_PRODUCTION_2026_09_20"
 PASSING_YARDS_STEP4_VISIBLE_REPLACEMENT_RUNTIME = "STREAMLIT_V188_PASSING_YARDS_STEP4_VISIBLE_REPLACEMENT_2026_09_20"
+SITEWIDE_UNIVERSAL_SHELL_RUNTIME = "SITEWIDE_UNIVERSAL_SHELL_V1_2026_09_20"
 
 from sports_api.monster_performance_profiler_v1 import (
     PerformanceTrace,
@@ -278,6 +279,7 @@ try:
     _app_started = perf_counter()
     _bootstrap_started = perf_counter()
     from streamlit_memory_lazy_router_v188 import record_bootstrap_import_ms, render_app
+    from kyre_universal_shell_runtime_v1 import activate_universal_shell
     # Frozen V187 delegate compatibility: from streamlit_memory_lazy_router_v187 import record_bootstrap_import_ms, render_app
     # Frozen V186 delegate compatibility: from streamlit_memory_lazy_router_v186 import record_bootstrap_import_ms, render_app
     # Frozen V185 delegate compatibility: from streamlit_memory_lazy_router_v185 import record_bootstrap_import_ms, render_app
@@ -305,6 +307,7 @@ try:
     _bootstrap_import_ms = (perf_counter() - _bootstrap_started) * 1000.0
     record_bootstrap_import_ms(_bootstrap_import_ms)
     run_streamlit_activation_probe()
+    activate_universal_shell()
     render_app()
     _app_total_ms = (perf_counter() - _app_started) * 1000.0
 
