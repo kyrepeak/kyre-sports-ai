f7789033924ca275a71146f54070cfab74b3ab77
kyrepeak <kyrewhite15@gmail.com>
2026-09-20 17:07:43 -0700

    Boot production through Passing Yards Router V188

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from time import perf_counter
 from typing import TYPE_CHECKING
 
 V186_PRODUCTION_RUNTIME_REFRESH = "STREAMLIT_V186_NEXT_GAME_DATE_RUNTIME_REFRESH_2026_09_20"
-NFL_STEP7_APP_IDENTITY_RUNTIME = "STREAMLIT_V187_NFL_STEP7_APP_IDENTITY_FAIL_CLOSED_2026_09_20"
+NFL_STEP7_APP_IDENTITY_RUNTIME = "STREAMLIT_V187_NFL_STEP7_APP_IDENTITY_FAIL_CLOSED_2026_09_20"\nPASSING_YARDS_LAYERED_PRODUCTION_RUNTIME = "STREAMLIT_V188_PASSING_YARDS_LAYERED_PRODUCTION_2026_09_20"
 
 from sports_api.monster_performance_profiler_v1 import (
     PerformanceTrace,
@@ -275,7 +275,7 @@ DEPLOYMENT_HEARTBEAT = "STREAMLIT_MAIN_V136_NFL_SPREAD_FRESH_ROUTER_2026-09-14"
 try:
     _app_started = perf_counter()
     _bootstrap_started = perf_counter()
-    from streamlit_memory_lazy_router_v187 import record_bootstrap_import_ms, render_app
+    from streamlit_memory_lazy_router_v188 import record_bootstrap_import_ms, render_app\n    # Frozen V187 delegate compatibility: from streamlit_memory_lazy_router_v187 import record_bootstrap_import_ms, render_app
     # Frozen V186 delegate compatibility: from streamlit_memory_lazy_router_v186 import record_bootstrap_import_ms, render_app
     # Frozen V185 delegate compatibility: from streamlit_memory_lazy_router_v185 import record_bootstrap_import_ms, render_app
     # Frozen V184 delegate compatibility: from streamlit_memory_lazy_router_v184 import record_bootstrap_import_ms, render_app
